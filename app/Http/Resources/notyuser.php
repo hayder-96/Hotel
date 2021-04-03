c40bd739e5eae725f7227f9cf77f06b2a31bbaba
hayder ali <hxhwe1924@gmail.com>
2021-04-04 00:30:00 +0300

    Procfile for Heroku

--- app/Http/Resources/notyuser.php
+++ app/Http/Resources/notyuser.php
@@ -24,8 +24,8 @@ class notyuser extends JsonResource
         'user_id'=>$this->user_id,
         'namehotel'=>$this->namehotel,
         'noty'=>$this->noty,
-        'created_at'=>$this->created_at->format('d/m/Y H:m:s'),
-        'updated_at'=>$this->updated_at->format('y/M/d H:m:s'),
+        'created_at'=>$this->created_at->format('d/m/Y H:M:s'),
+        'updated_at'=>$this->updated_at->format('y/m/d H:M:s'),
 
     
    ];
