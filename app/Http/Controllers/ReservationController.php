f4a87061597d8e0396bd683bd08f7171af9d063f
hayder ali <hxhwe1924@gmail.com>
2021-04-05 16:06:25 +0300

    Procfile for Heroku

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -54,36 +54,36 @@ class ReservationController extends BaseController
         $idi=InformHotel::find($request->id);
         $input=$request->all();
 
-        // $valdit=Validator::make($request->all(),[
-        //     'name',
-        //     'phone',             
-        //     'email',
-        //     'country',
-        //     'typetrip',
-        //     'typeroom',
-        //     'nameroom',
-        //     'priceroom',
-        //     'access',
-        //     'leaving',
-        //     'typebed',
-        //     'numbed',
-        //     'numguest',
-        //     'noty',
-        //     'admin_id'
+        $valdit=Validator::make($request->all(),[
+            'name',
+            'phone',             
+            'email',
+            'country',
+            'typetrip',
+            'typeroom',
+            'nameroom',
+            'priceroom',
+            'access',
+            'leaving',
+            'typebed',
+            'numbed',
+            'numguest',
+            'noty',
+            'admin_id'
 
-        //     ]);
+            ]);
 
-        //     if($valdit->fails()){
+            if($valdit->fails()){
     
-        //         return $this->sendError('Failed input',$valdit->errors());
-        //     }
+                return $this->sendError('Failed input',$valdit->errors());
+            }
 
 
-            // $input['user_id']=Auth::id();
-            // $input['namehotel']=$idi->namehotel;
+            $input['user_id']=Auth::id();
+            $input['namehotel']=$idi->namehotel;
 
-            $hotel=$idi->namehotel;
-          //  $hotel=reservation::create($input);
+      
+            $hotel=reservation::create($input);
 
             return $this->Respone($hotel,200);
     }
