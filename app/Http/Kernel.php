ff08533a8c847127e12ad04396180fa6159af47d
hayder ali <hxhwe1924@gmail.com>
2021-04-18 13:39:11 +0300

    Procfile for Heroku

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,9 +40,8 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:60,1',
-            'bindings',
-           // \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            'throttle:api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
 
